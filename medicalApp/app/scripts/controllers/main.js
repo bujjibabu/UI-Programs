8c20f8fd02bbdac9b75447a95865fdb74848f9eb
bujjibabu <bujji404@gmail.com>
2018-01-31 07:55:48 +0530

    angular app

---
+++ medicalApp/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name medicalApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the medicalApp
+ */
+angular.module('medicalApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
